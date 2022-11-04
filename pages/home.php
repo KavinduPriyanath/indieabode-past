f09f19f31cb215492844fda829e2df790b0557c0
Kavindu <kavindupriyanath@gmail.com>
2022-11-04 15:57:24 +0530

    added homepage

---
+++ pages/home.php
@@ -0,0 +1,208 @@
+<!--Games-->
+
+<div class="topic">
+    Games
+    <i class="fa fa-angle-double-right"></i>
+</div>
+
+<hr id="topic-break" />
+
+<div class="container" id="card-container">
+    <div class="card">
+        <div class="card-image game-1"></div>
+        <div class="game-intro">
+            <h3>Riley & Rochelle</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            A narrative puzzle game about love and pop in the 90s
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-image game-2"></div>
+        <div class="game-intro">
+            <h3>Late Night Mop</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">A haunted house cleaning simulators</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image game-3"></div>
+        <div class="game-intro">
+            <h3>The Spirit and the Mouse</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            Bring kindness and light to the people of Sainte-et-Claire as Lila—a
+            tiny mouse with a big heart!
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-image game-4"></div>
+        <div class="game-intro">
+            <h3>World of Anterra</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            An innovative, open world RPG, influenced by classic and modern games.
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-image game-1"></div>
+        <div class="game-intro">
+            <h3>Riley & Rochelle</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            A narrative puzzle game about love and pop in the 90s
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-image game-2"></div>
+        <div class="game-intro">
+            <h3>Late Night Mop</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">A haunted house cleaning simulators</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image game-3"></div>
+        <div class="game-intro">
+            <h3>The Spirit and the Mouse</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            Bring kindness and light to the people of Sainte-et-Claire as Lila—a
+            tiny mouse with a big heart!
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-image game-4"></div>
+        <div class="game-intro">
+            <h3>World of Anterra</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            An innovative, open world RPG, influenced by classic and modern games.
+        </div>
+    </div>
+</div>
+
+<!--Assets-->
+
+<div class="topic">
+    Assets
+    <i class="fa fa-angle-double-right"></i>
+</div>
+
+<hr id="topic-break" />
+
+<div class="container" id="card-container">
+    <div class="card">
+        <div class="card-image asset-1">
+            <div class="asset-type">Skybox</div>
+        </div>
+        <div class="game-intro">
+            <h3>Skybox Pack Unity</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">Consists of 34 skyboxes</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image asset-2">
+            <div class="asset-type">Pixel Art</div>
+        </div>
+        <div class="game-intro">
+            <h3>Tiny Sword</h3>
+            <p>$10.99</p>
+        </div>
+        <div class="tagline">The most deadly and colorful battle</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image asset-3">
+            <div class="asset-type">Tile-Set</div>
+        </div>
+        <div class="game-intro">
+            <h3>Kings and Pigs</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">A king vs an army of cute small pigs</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image asset-4">
+            <div class="asset-type">Character</div>
+        </div>
+        <div class="game-intro">
+            <h3>Simpsons and Sons</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            A pack with everything you need to make a 3D platformer! quaternius
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-image asset-1">
+            <div class="asset-type">Skybox</div>
+        </div>
+        <div class="game-intro">
+            <h3>Skybox Pack Unity</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">Consists of 34 skyboxes</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image asset-2">
+            <div class="asset-type">Pixel Art</div>
+        </div>
+        <div class="game-intro">
+            <h3>Tiny Sword</h3>
+            <p>$10.99</p>
+        </div>
+        <div class="tagline">The most deadly and colorful battle</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image asset-3">
+            <div class="asset-type">Tile-Set</div>
+        </div>
+        <div class="game-intro">
+            <h3>Kings and Pigs</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">A king vs an army of cute small pigs</div>
+    </div>
+
+    <div class="card">
+        <div class="card-image asset-4">
+            <div class="asset-type">Character</div>
+        </div>
+        <div class="game-intro">
+            <h3>Simpsons and Sons</h3>
+            <p>$19.99</p>
+        </div>
+        <div class="tagline">
+            A pack with everything you need to make a 3D platformer! quaternius
+        </div>
+    </div>
+</div>
+
+<!--Gigs-->
+
+<div class="topic">
+    Gigs
+    <i class="fa fa-angle-double-right"></i>
+</div>
+
+<hr id="topic-break" />
