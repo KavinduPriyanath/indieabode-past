21a9da56b241a114533ad2ea061b13126b1e1268
Kavindu <kavindupriyanath@gmail.com>
2022-11-08 13:33:13 +0530

    Changed Db files and routes

--- db/database.php
+++ db/database.php
@@ -1,6 +1,12 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'indieabode');
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "indieabode";
+
+
+$conn = mysqli_connect($host, $username, $password, $db_name);
 
 if (!$conn) {
     echo "Couldn't connect" . mysqli_connect_error();
