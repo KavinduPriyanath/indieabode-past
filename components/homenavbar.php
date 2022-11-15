74d361b0c6d3db872296a62e2919b5288c4f0941
Kavindu <kavindupriyanath@gmail.com>
2022-11-15 16:03:57 +0530

    Added Redactor Box- But not saving to db

--- components/homenavbar.php
+++ components/homenavbar.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
     <style>
-        <?php include '../src/css/navbar.css'; ?>
+        <?php include 'src/css/navbar.css'; ?>
     </style>
 
     <title>IndieAbode</title>
